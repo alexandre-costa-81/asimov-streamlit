a08c891555f753cffa6c8c796f9e1e7f4c7834c3
Alexandre Costa <alexandre.costa@furg.br>
2025-06-13 21:05:17 -0300

    Refactor main function to set DataFrame index to 'Track' and enable artist and album selection for visualizing streams

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import streamlit as st
 import pandas as pd
 
+
+st.set_page_config(
+    page_title="Spotify Streamers",
+    page_icon=":musical_note:",
+    layout="wide",
+)
+
 def main():
     df = pd.read_csv("01 Spotify.csv")
 
-    df.set_index("Artist", inplace=True)
-    st.bar_chart(df[df["Stream"] > 1000000000]["Stream"])
+    df.set_index("Track", inplace=True)
+
+    artists = df["Artist"].value_counts().index
+    artist = st.selectbox("Artista", artists)
+    df_artist = df[df["Artist"] == artist]
+
+    albums = df_artist["Album"].value_counts().index
+    album = st.selectbox("Álbum", albums)
+    df_album = df[df["Album"] == album]
+
+    display = st.checkbox("Display")
+
+    if display:
+        st.bar_chart(df_album["Stream"])
 
 
 if __name__ == "__main__":
